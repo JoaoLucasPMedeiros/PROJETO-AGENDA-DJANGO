9873146fa4eff1d9dabe0fa79b251f26265e1485
Joao Lucas <joaolucaspmedeiros@gmail.com>
2024-05-01 22:48:23 -0300

    Finalizando toda parte do model

--- contact/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-""" ESTÁ FUNÇÃO ESTÁ RENDERINADO O INDEX.HMTL """
-def index(request):
-     return render(
-          request,
-          'contact/index.html'
-     )
---
+++ project/local_settings.py
@@ -0,0 +1,6 @@
+from project.local_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS
+
+
+DEBUG = True
+SECRET_KEY = 's'
+ALLOWED_HOSTS:list[str] = []
